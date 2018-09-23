1e9c52df8a8684686909964ec05c4069be3070f1
Solomon <solomonyu1@gmail.com>
2018-09-23 15:34:14 -0700

    PANIC

--- server.js
+++ server.js
@@ -218,7 +218,7 @@ function sortLeaderboard(req,res,next,userArray){
 function loadThisThing(req,res,next,toLoad){
   console.log(toLoad);
   console.log("above object loaded");
-  res.send(a);
+  res.send(toLoad);
   res.end();
 }
 
